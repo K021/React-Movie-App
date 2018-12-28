73b50bf4d303b61e758c362e0bebe71e2f61390f
Kim Jooeon <joo2theeon@gmail.com>
2018-12-29 01:42:33 +0900

    add get Api feature with fetch, async, await

--- src/App.js
+++ src/App.js
@@ -5,45 +5,30 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
+  state = {};
 
-  };
-
+  // 요녀석은 가벼운 것이 좋다. 많은 일을 해야 하기 때문에. 그래서 함수를 쪼갰다.
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: 'Inception',
-            poster: 'https://cdn.shopify.com/s/files/1/1416/8662/products/inception_2010_french_original_film_art_spo_2000x.jpg?v=1543425422',
-          },
-
-          {
-            title: 'Transcendence',
-            poster: 'https://i.pinimg.com/originals/42/56/6d/42566daff7cfe843d51780e73802a83c.jpg',
-          },
-
-          {
-            title: 'Interstellar',
-            poster: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg',
-          },
-
-          {
-            title: 'Catch me if you can',
-            poster: 'https://cdn.shopify.com/s/files/1/1416/8662/products/catch_me_if_you_can_2002_original_film_art_spo_2000x.jpg?v=1543418719',
-          },
-          {
-            title: 'Borne Identity',
-            poster: 'https://m.media-amazon.com/images/M/MV5BM2JkNGU0ZGMtZjVjNS00NjgyLWEyOWYtZmRmZGQyN2IxZjA2XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg',
-          },
-        ]
-      })
-    }, 3000);
+    this._getMovies();
   }
 
+  async _getMovies() {  // async 비동기 함수
+    const movies = await this._callApi();  // await: 비동기 함수의 종료를 기다린다. 성공 여부는 상관없다.
+    this.setState({movies})  // movies: movies 가 된다.
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')  // 반환해줘야 한다.
+      .then(response => response.json())
+      .then(jsonData => jsonData.data.movies)  // 요 값을 반환
+      .catch(err => console.log(err))
+  };
+
   _renderMovies = () => {
-    return this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    return this.state.movies.map(movie => {
+      // movie 객체를 내가 만든 것이 아니므로, Api json data 형태를 잘 보고 사용해줘야 한다.
+      // key 로 영화의 id 값을 사용할 수 있다. 인덱스를 사용하는 것 보다 빠르다고? 한다
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id}/>
     });
   };
 
